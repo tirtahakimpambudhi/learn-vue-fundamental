72941bd05f42de66853f0682bc26b07f5668d782
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-28 14:55:50 +0700

    chore: Update form.ts files

--- src/handlers/form.ts
+++ src/handlers/form.ts
@@ -1,7 +1,21 @@
 import { userState } from "@states/user";
+import { isSubmitState} from "@states/isSubmit.ts";
+import {personState} from "@states/person.ts";
+
+export const addUser: ( _: Event ) => void = (_) => {
+    isSubmitState.value = true;
+    console.log(JSON.stringify(userState, null, 2));
+    // Simulate Call API
+    setTimeout(() => {
+        isSubmitState.value = false;
+    }, 3000)
+};
 
 
-export const addUser = (event : SubmitEvent) => {
-    console.log(event);
-    console.log(userState);
+export const addPerson: ( _: Event ) => void = (_) => {
+    isSubmitState.value = true;
+    console.log(JSON.stringify(personState, null, 2));
+    setTimeout(() => {
+        isSubmitState.value = false;
+    }, 3000)
 }
