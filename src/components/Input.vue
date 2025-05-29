45ccf1fdcadc8955d6bf59967d05e8968101da27
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-29 18:08:06 +0700

    feat(components): :sparkles: Update components in the file Input.vue

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -48,8 +48,6 @@ const modelValue = getUserState(key as keyof typeof userState);
           :type="props.typeInput"
           :name="key"
           :id="key"
-          ahh ah suki yess
-          penjual balon!!!!
           v-model="modelValue"
           class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
         <!--          @change="onChange"-->
