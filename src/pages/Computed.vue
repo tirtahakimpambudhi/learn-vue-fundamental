c97dee45f5dd04508ee061b1ee2fdb3bf52a6f21
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-30 14:55:51 +0700

    chore: Add Computed.vue files

---
+++ src/pages/Computed.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+  import {personState} from "@states/person";
+  import {isSubmitState} from "../states/isSubmit.ts";
+  import Input from "@components/Input.vue";
+
+  const fullNameNonComputed = () => {
+    console.log('non computed function call')
+    return `${personState.firstName} ${personState.lastName}`
+  }
+
+  const fullNameComputed = () => {
+    console.log('computed function call')
+    return `${personState.firstName} ${personState.lastName}`
+  }
+
+</script>
+
+<template>
+  <form>
+    <Input label="FirstName" type-input="text"/>
+    <Input label="LastName" type-input="text"/>
+    <button type="submit" >
+      {{ isSubmitState ? 'Submitting...' : 'Submit' }}
+    </button>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
