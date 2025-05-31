f42207c02c54dd2616c570f4dfd6e30e2dfda6e9
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-31 14:55:51 +0700

    chore: Update Form.vue files

--- src/pages/Form.vue
+++ src/pages/Form.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import Input from '@components/Input.vue';
 import {addUser} from '@handlers/form';
+import {isSubmitState} from "../states/isSubmit.ts";
 
 </script>
 
 <template>
-    <form @submit.prevent="addUser">
-        <Input :label="'name'" :typeInput="'text'"></Input>
-        <button type="submit" >Kirim</button>
+    <form class="" @submit.prevent="(e) => addUser(e)">
+        <Input :label="'Name'" :typeInput="'text'"></Input>
+        <Input :label="'Email'" :typeInput="'email'"></Input>
+
+        <button type="submit" :disabled="isSubmitState">
+          {{ isSubmitState ? 'Submitting...' : 'Submit' }}
+        </button>
     </form>
 </template>
 
