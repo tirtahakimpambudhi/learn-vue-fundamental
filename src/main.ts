c546627f2279694090ea5aa6c0c7a5b222d6d595
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-31 13:49:26 +0700

    chore: Add main.ts files

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import './style.css'
+// import App from '@pages/App.vue'
+//
+// createApp(App).mount('#app')
+
+// import Composition from "@pages/Composition.vue";
+//
+// createApp(Composition).mount('#app')
+
+
+import TypesData from "@pages/TypesData.vue";
+
+createApp(TypesData).mount(
+    '#app'
+)
