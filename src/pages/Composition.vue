0c6f16e9cf37e5cb5c61bb404a7f1ebfb0b8d64d
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-02 13:49:28 +0700

    chore: Add Composition.vue files

---
+++ src/pages/Composition.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import ButtonIncrement from "@components/ButtonIncrement.vue";
+import {ref} from "vue";
+
+const count = ref<number>(0)
+
+</script>
+
+<template>
+<!--  TODO: FIX THISS -->
+  <ButtonIncrement :count="ref(0)" @increment="() => {count++}" />
+</template>
+
+<style scoped>
+
+</style>
