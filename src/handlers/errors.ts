a0e6423207fa7b93e536734a8bc0d2d4cb4bcc8e
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-02 18:20:29 +0700

    feat(handlers): :sparkles: Add handlers in the file errors.ts

---
+++ src/handlers/errors.ts
@@ -0,0 +1,23 @@
+
+
+import { z } from "zod";
+
+export function handleZodError(
+    error: z.ZodError,
+    errorState: Partial<Record<string, string>>
+) {
+    Object.keys(errorState).forEach((key) => {
+        errorState[key] = undefined;
+    });
+
+    console.error(error)
+
+
+    error.errors.forEach((err) => {
+        const field = err.path[0];
+        const message = err.message;
+        if (typeof field === "string") {
+            errorState[field] = message;
+        }
+    });
+}
