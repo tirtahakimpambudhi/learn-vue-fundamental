eeaebe1c8c6697c1223f3f13de8a5defc112d346
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-03 18:08:09 +0700

    chore: Add tailwind.config.ts files

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+
+const config = {
+    content: [
+        './index.html',
+        './src/**/*.{ts,tsx,js,jsx,html}',
+    ],
+        theme: {
+            extend: {
+                colors: {
+                    primary: {
+                        50: '#f0f9ff',
+                        500: '#3b82f6',
+                        600: '#2563eb',
+                        700: '#1d4ed8'
+                    }
+                }
+            }
+        },
+    plugins: [],
+}
+export default config
