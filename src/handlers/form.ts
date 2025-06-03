d5937bf6d2c5b9bf20f12d936002312e5266c7df
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-03 18:20:29 +0700

    feat(handlers): :sparkles: Update handlers in the file form.ts

--- src/handlers/form.ts
+++ src/handlers/form.ts
@@ -2,7 +2,14 @@ import { userState } from "@states/user";
 import { isSubmitState} from "@states/isSubmit";
 import {personState} from "@states/person";
 import {isSubmitTodoState} from "@states/isSubmitTodo";
-import {todoListState} from "@states/todoList";
+import {z} from "zod";
+import {handleZodError} from "@handlers/errors";
+import useTodolist from "@states/useTodolist";
+import {todoErrorState} from "@states/todoError";
+import {addTodoValidation} from "@/validation/todo";
+import {todoItemState} from "@states/todoitem";
+import type {TodoItem} from "@/types/state";
+import {v4 as uuidV4} from "uuid"
 
 export const addUser: ( _: Event ) => void = (_) => {
     isSubmitState.value = true;
@@ -23,11 +30,37 @@ export const addPerson: ( _: Event ) => void = (_) => {
 }
 
 
-export const addTodo : (_: Event) => void = () => {
+export const addTodo = async (_ : Event) => {
     isSubmitTodoState.value = true;
     try {
-        todoListState.push()
+        // Validation The form
+        addTodoValidation.parse(todoItemState)
+        todoItemState.id = uuidV4()
+        // create the todo to api
+        await useTodolist().createTodolist({...todoItemState})
+
+        // Reset todo item
+        Object.assign(todoItemState, {
+            id: "",
+            title: "",
+            category: "Belajar",
+            priority: "Sedang",
+            description: "",
+            deadline: Date.now(),
+            created_at: Date.now(),
+            updated_at: Date.now(),
+        }  as TodoItem)
+
+        Object.keys(todoErrorState).forEach((key) => {
+            todoErrorState[key as keyof typeof todoErrorState] = undefined;
+        });
+
     } catch (error) {
-        console.log(error);
+        if ( error instanceof z.ZodError ) {
+            handleZodError(error, todoErrorState)
+        }
+        console.log(error instanceof z.ZodError, typeof error, error)
+    } finally {
+       isSubmitTodoState.value = false;
     }
 }
