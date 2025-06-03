4b863896119f7302e0a2aac96b71238281808b1b
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-03 13:49:28 +0700

    chore: Add TypesData.vue files

---
+++ src/pages/TypesData.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+  const number : number = 1;
+  const string : string = "this is a string";
+  const boolean : boolean = true;
+
+  const arr : string[] = ["a", "b", "c"];
+  const arrReadOnly :ReadonlyArray<string,3> = ["a","b","c"];
+
+  arrReadOnly.push("d");
+  console.log(arrReadOnly);
+</script>
+
+<template>
+  <h1> Type Data Number</h1>
+  <p> Value for variable number  : {{ number }} </p>
+</template>
+
+<style scoped>
+
+</style>
