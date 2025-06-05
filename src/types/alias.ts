8ddd7b414a12bc9de3c1dc2cbca848b777ad4815
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-05 14:55:54 +0700

    chore: Update alias.ts files

--- src/types/alias.ts
+++ src/types/alias.ts
@@ -1,3 +1,5 @@
 export type myTuple = readonly [string, string, number, boolean];
 
-export type typeData = string | number | boolean | any[] | myTuple;
+export type typeData = string | number | boolean | any[] | myTuple | ReadonlyArray<any>;
+
+export type SubmitHandler = (event: Event) => void;
