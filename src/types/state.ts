97b8dac684b1b9d64ea24acdb464775f1f3a9a55
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-06 14:55:54 +0700

    chore: Update state.ts files

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,7 +1,23 @@
 export type Gender = 'female' | 'male';
 
+
 export type user = {
-    name: string,
-    email: string,
-    gender: Gender
+    name: string;
+    email: string;
+    gender: Gender;
 };
+
+export type Person = {
+    firstName: string;
+    lastName: string;
+}
+
+
+
+
+// INTERFACE
+// CANNOT PRIMITIVE TYPE
+// CAN MERGE OR OVERRIDE
+// Native Support Extends
+
+
