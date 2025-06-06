e3e9a276a2564eac13825c98954b7fa80be90b27
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-06 18:20:31 +0700

    feat(states): :sparkles: Update states in the file todoError.ts

--- src/states/todoError.ts
+++ src/states/todoError.ts
@@ -4,4 +4,9 @@ import {reactive} from "vue";
 
 
 export const todoErrorState :Reactive<TodoError> = reactive<TodoError>({
+    title: undefined,
+    category: undefined,
+    priority: undefined,
+    deadline: undefined,
+    description: undefined,
 });
