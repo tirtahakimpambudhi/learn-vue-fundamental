245f6fc4b8b3ca4333026303d51e9eff13bc87dd
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-08 18:20:32 +0700

    feat(types): :sparkles: Update types in the file alias.ts

--- src/types/alias.ts
+++ src/types/alias.ts
@@ -3,3 +3,5 @@ export type myTuple = readonly [string, string, number, boolean];
 export type typeData = string | number | boolean | any[] | myTuple | ReadonlyArray<any>;
 
 export type SubmitHandler = (event: Event) => void;
+
+export type TodoAction = 'getAll' | 'getById' | 'create' | 'update' | 'destroy';
