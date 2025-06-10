6514285eacda1b46214ef540238e6b66b9ffa864
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-10 18:20:33 +0700

    feat(validation): :sparkles: Update validation in the file todo.ts

--- src/validation/todo.ts
+++ src/validation/todo.ts
@@ -1,8 +1,20 @@
-import { z } from "zod/v4"
-import type {TodoItem} from "@/types/state";
+import { z } from "zod"
 
-
-export const addTodoValidation = z.object<TodoItem>({
-    title: z.string().
+export const addTodoValidation = z.object({
+    title: z
+        .string({ message: "Judul wajib diisi" })
+        .min(1, { message: "Judul tidak boleh kosong" }),
+    category: z.enum(["Pribadi", "Belajar", "Pekerjaan", "Kesehatan"], { message: "Kategori wajib diisi" }),
+    priority: z.enum(["Rendah", "Sedang", "Tinggi", "Urgent"], { message: "Prioritas wajib dipilih" }),
+    description: z
+        .string({ message: "Deskripsi wajib diisi" })
+        .min(1, { message: "Deskripsi tidak boleh kosong" }),
+    deadline: z
+        .number({ message: "Batas waktu wajib diisi" })
+        .nonnegative({ message: "Batas waktu tidak boleh negatif" }),
+    created_at: z
+        .number({ message: "Tanggal dibuat wajib diisi" }),
+    updated_at: z
+        .number({ message: "Tanggal diperbarui wajib diisi" })
 })
 
