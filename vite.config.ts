b12a1d3108c8ca66f54ed4069b8082d3965517a7
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-12 13:49:33 +0700

    chore: Add vite.config.ts files

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+      vue(),
+      tailwindcss(),
+  ],
+    resolve: {
+      alias: {
+          "@": resolve(__dirname, "./src"),
+          "@components": resolve(__dirname, "./src/components"),
+          "@assets": resolve(__dirname, "./src/assets"),
+          "@types": resolve(__dirname,"./src/types"),
+          "@pages": resolve(__dirname, "./src/pages"),
+      }
+    }
+})
