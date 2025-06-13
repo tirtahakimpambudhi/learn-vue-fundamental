5f40fd02d4e2b5096a5083f1591a975582a2b3d3
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-13 16:02:40 +0700

    feat(pages): :sparkles: Add pages in the file Composition.vue

---
+++ src/features/learn/pages/Composition.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import ButtonIncrement from "@learn/components/ButtonIncrement.vue";
+import {ref} from "vue";
+
+const count = ref<number>(0)
+
+</script>
+
+<template>
+  <ButtonIncrement :count="count" @increment="() => {count++}" />
+</template>
+
+<style scoped>
+
+</style>
