6d8ecd721a8a76b3686a6f9947655fde5b12ce06
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-15 16:02:42 +0700

    feat(pages): :sparkles: Add pages in the file Form.vue

---
+++ src/features/learn/pages/Form.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Input from '@learn/components/Input.vue';
+import {addUser} from '@learn/handlers/form';
+import {isSubmitState} from "@learn/states/isSubmit";
+
+</script>
+
+<template>
+    <form class="" @submit.prevent="(e) => addUser(e)">
+        <Input :label="'Name'" :typeInput="'text'"></Input>
+        <Input :label="'Email'" :typeInput="'email'"></Input>
+
+        <button type="submit" :disabled="isSubmitState">
+          {{ isSubmitState ? 'Submitting...' : 'Submit' }}
+        </button>
+    </form>
+</template>
+
+<style scoped></style>
