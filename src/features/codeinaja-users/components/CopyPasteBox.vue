a33fdb6bf36e7550e609dbca9ed8b1c050759297
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-15 14:50:20 +0700

    feat(components): :sparkles: Update components in the file CopyPasteBox.vue

--- src/features/codeinaja-users/components/CopyPasteBox.vue
+++ src/features/codeinaja-users/components/CopyPasteBox.vue
@@ -13,12 +13,12 @@ const srcImage = computed(() => {
 </script>
 
 <template>
-    <div className="mb-4">
-      <div className="bg-white border-2 border-dashed border-[#D1D5DB] text-sm px-3 py-2 inline-block rounded text-[#6B7280]">
-        <div className="flex gap-4 items-center">
+    <div class="mb-4">
+      <div class="bg-white border-2 border-dashed border-[#D1D5DB] text-sm px-3 py-2 inline-block rounded text-[#6B7280]">
+        <div class="flex gap-4 items-center">
           <strong>FOKUSCODING</strong>
-          <button @click="handleCopied('FOKUSCODING')"  className="hover:opacity-75 active:scale-95 transition cursor-pointer">
-            <img :src="srcImage" alt="icon" className="w-5 h-5" />
+          <button @click="handleCopied('FOKUSCODING')"  class="hover:opacity-75 active:scale-95 transition cursor-pointer">
+            <img :src="srcImage" alt="icon" class="w-5 h-5" />
           </button>
         </div>
       </div>
