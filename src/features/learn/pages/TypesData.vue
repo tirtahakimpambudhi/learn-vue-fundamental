c6ef2b30fe4ac6a72a6ba9df5f6fb7f96db000dc
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-16 16:02:43 +0700

    feat(pages): :sparkles: Add pages in the file TypesData.vue

---
+++ src/features/learn/pages/TypesData.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import TypeData from '@learn/components/TypeData.vue';
+  import { type myTuple } from '@learn/types/alias';
+
+  const myNumber : number = 1;
+  const myString : string = "this is a string";
+  const myBoolean : boolean = true;
+
+  const myArr : string[] = ["a", "b", "c"];
+  const tuple :myTuple = ["foo", "bar", 100, true];
+
+  const arrReadOnly :ReadonlyArray<string> = ["a", "b", "c"];
+
+</script>
+
+<template>
+  <TypeData :data="myNumber" :nameVariable="'myNumber'" />
+  <br>
+  <TypeData :data="myString" :nameVariable="'myString'" />
+  <br>
+  <TypeData :data="myBoolean" :nameVariable="'myBoolean'" />
+  <br>
+  <TypeData :data="myArr" :nameVariable="'myArr'" />
+  <br>
+  <TypeData :data="tuple" :nameVariable="'tuple'" />
+  <br>
+  <TypeData :data="arrReadOnly" :nameVariable="'arrReadOnly'" />>
+</template>
+
+<style scoped>
+
+</style>
