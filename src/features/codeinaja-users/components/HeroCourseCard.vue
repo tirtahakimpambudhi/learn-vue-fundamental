ec5c8e1c177850e816a8015b7f8608d28bd70f8c
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-19 14:50:23 +0700

    feat(components): :sparkles: Update components in the file HeroCourseCard.vue

--- src/features/codeinaja-users/components/HeroCourseCard.vue
+++ src/features/codeinaja-users/components/HeroCourseCard.vue
@@ -2,8 +2,6 @@
 import useHeroCourseCard from '../states/useHeroCourseCard';
 import type { HeroCourseCard } from '../types/HeroCourseCard';
 
-
-
 const props = defineProps<{
     card: HeroCourseCard
 }>();
@@ -13,21 +11,21 @@ const cardState = useHeroCourseCard(props.card);
 </script>
 
 <template>
-    <div className="bg-white border border-[#F3F4F6] rounded-xl overflow-hidden w-full lg:w-1/2">
-    <div className="p-6">
-        <div className="flex justify-between items-center mb-3">
-        <h3 className="text-orange-600 font-semibold text-lg">
+    <div class="bg-white border border-[#F3F4F6] rounded-xl overflow-hidden w-full lg:w-1/2">
+    <div class="p-6">
+        <div class="flex justify-between items-center mb-3">
+        <h3 class="text-orange-600 font-semibold text-lg">
             {{ cardState.card.title }}
         </h3>
-        <span v-if="cardState.card.percentDiscon" className="bg-orange-100 text-orange-600 text-xs font-bold px-2 py-1 rounded">
-            {{ cardState.card.percentDiscon }}% OFF
+        <span v-if="cardState.card.percentDiscount" class="bg-orange-100 text-orange-600 text-xs font-bold px-2 py-1 rounded">
+            {{ cardState.card.percentDiscount }}% OFF
         </span>
         </div>
-        <p className="text-sm text-gray-400" :class="cardState.card.percentDiscon ? 'line-through' : ''">
+        <p class="text-sm text-gray-400" :class="cardState.card.percentDiscount ? 'line-through' : ''">
             {{ cardState.card.price.toLocaleString(cardState.card.currency) }}
         </p>
-        <p v-if="cardState.card.percentDiscon" className="text-xl font-bold text-orange-500 mb-4">
-        {{ cardState.disconPrice.value.toLocaleString(cardState.card.currency) }}
+        <p v-if="cardState.card.percentDiscount" class="text-xl font-bold text-orange-500 mb-4">
+        {{ cardState.discountPrice.value.toLocaleString(cardState.card.currency) }}
         </p>
         <img :src="cardState.card.banner" :alt="cardState.card.title" />
     </div>
