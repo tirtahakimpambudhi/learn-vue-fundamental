36d5d4a2cc3f0bd588f69714a70eac2dc30b26c7
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-24 14:50:27 +0700

    feat(pages): :sparkles: Update pages in the file App.vue

--- src/features/codeinaja-users/pages/App.vue
+++ src/features/codeinaja-users/pages/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import Navbar from "@codeinaja-users/components/Navbar.vue";
+import Footer from "@codeinaja-users/components/Footer.vue";
 
 
 </script>
 
 <template>
+    <Navbar></Navbar>
     <RouterView></RouterView>
+  <Footer></Footer>
 </template>
 
 <style scoped>
