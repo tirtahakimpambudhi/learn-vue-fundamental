5612d6e18a3edfbb43f0257d4e7dcb838dd2fec0
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-27 14:50:29 +0700

    feat(states): :sparkles: Update states in the file useAPI.ts

--- src/features/codeinaja-users/states/useAPI.ts
+++ src/features/codeinaja-users/states/useAPI.ts
@@ -1,23 +1,58 @@
-import useLoading from "@states/useLoading";
-import { reactive } from "vue"
+import profileOne from '@assets/profile-1.png'
+import useLoading from "@states/useLoading"
+import { ref } from "vue"
+import { type User } from "@codeinaja-users/types/User"
 
 export default function useAPI() {
-    const user = reactive({value: false});
-    const { isLoading, setLoading } = useLoading(false)
-    const loadUser = () => {
-        setLoading(true);
-        try {
-            user.value = true;
-        } catch (error) {
-            console.error(error)
-        } finally {
-            setLoading(false)
+  const { isLoading, setLoading } = useLoading(false)
+
+  const user = ref<User | null>(null)
+
+  const setUser = (value : User) => {
+    user.value = {
+        ...value
+    }
+  }
+
+  const loadUser = async () => {
+    setLoading(true)
+    try {
+      await new Promise(resolve => setTimeout(resolve, 1500))
+
+      setUser({
+        id: Date.now().toString(),
+        email: "johndoe@gmail.com",
+        username: "johndoe",
+        role: "student",
+        profile: {
+          about: "lorem ipsum",
+          gender: "male",
+          photo: profileOne
         }
+      })
+    } catch (error) {
+      console.error("Failed to load user:", error)
+    } finally {
+      setLoading(false)
     }
-    return {
-        // Dummy Data
-        user,
-        loadUser,
-         isLoading
+  }
+
+  const logout = async () => {
+    setLoading(true);
+    try {
+        user.value = null;
+    } catch (error) {
+        console.error("Failed to logout user:", error);
+    } finally {
+        setLoading(false);
     }
+  }
+
+  return {
+    user,
+    loadUser,
+    isLoading,
+    setLoading,
+    logout
+  }
 }
