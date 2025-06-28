6516314f0501d5a9f1f508ae5feebd94d791b0d3
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-28 14:50:30 +0700

    feat(states): :sparkles: Update states in the file useCopied.ts

--- src/features/codeinaja-users/states/useCopied.ts
+++ src/features/codeinaja-users/states/useCopied.ts
@@ -1,16 +1,12 @@
-import { ref } from "vue";
+import useToggle from "@/states/useToggle";
 
 export default function useCopied() {
-    const copied = ref<boolean>(false);
-
-    const setCopied = (value : boolean) => {
-        copied.value = value
-    }
+    const { state: copied, setState: setCopied, setTrue: startCopied, setFalse: endCopied } = useToggle(false);
 
     const handleCopied = (copyValue :string) => {
         navigator.clipboard.writeText(copyValue);
-        setCopied(true);
-        setTimeout(() => {setCopied(false)}, 1500);
+        startCopied()
+        setTimeout(() => {endCopied}, 1500);
     }
 
     return {copied, setCopied, handleCopied}
