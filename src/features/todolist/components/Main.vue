0e8db8a8bc9c1a8a46ee1632fc18e5b0a4ed9024
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-30 16:02:57 +0700

    feat(components): :sparkles: Add components in the file Main.vue

---
+++ src/features/todolist/components/Main.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+     <main class="max-w-4xl mx-auto px-6 py-8">
+
+    <!-- Form Component -->
+    <slot name="form">
+    
+    </slot>
+
+    <!-- Filter Component -->
+    <slot name="filter">
+
+    </slot>
+
+    <!-- Content Component -->
+    <slot name="content">
+
+    </slot>
+
+    <!-- Progress Component -->
+     <slot name="progress">
+        
+     </slot>
+
+
+     </main>
+</template>
+
+<style scoped>
+/* Your styles here */
+</style>
