c5c90c93b30b1d366eb400c05c0ae24532d41cd1
tirtahakimpambudhi <awebthp@gmail.com>
2025-07-06 16:03:04 +0700

    feat(states): :sparkles: Add states in the file useForm.ts

---
+++ src/features/todolist/states/useForm.ts
@@ -0,0 +1,86 @@
+import { reactive,ref } from "vue";
+import type { Item } from "@todolist/types/Item";
+import type { ZodError } from "@todolist/types/ZodError";
+import { z } from "zod";
+import { handleZodError } from "@todolist/handlers/error";
+import { schema } from "@todolist/validation/schema";
+import {v4 as uuidv4} from "uuid";
+import { useAPIStore } from "@todolist/stores/useAPIStore";
+import type { Entity } from "../types/Entity";
+
+export default function useForm() {
+
+    const useAPI = useAPIStore();
+
+    const form = reactive<Item>({
+        id: '',
+        title: '',
+        category: 'Belajar',
+        priority: 'Sedang',
+        description: '',
+        completed: false,
+        deadline: new Date(),
+        createdAt: new Date(),
+        updatedAt: null,
+    });
+
+    const errors = reactive<ZodError>({});
+
+    const isSubmit = ref<boolean>(false);
+
+    const onSubmit = async (_: Event) => {
+        isSubmit.value = true;
+        try {
+            // Validation the value form
+            schema.parse(form)
+
+            if (form.id) {
+                useAPI.updateTodolist(form.id, {...form} as Entity)
+            } else {
+                // Set the form.id
+                form.id = uuidv4()
+                // Create the todo
+                useAPI.createTodolist({
+                    ...form
+                } as Entity);
+            }
+
+            // Reset state todo form
+            Object.assign(form, {
+                id: '',
+                title: '',
+                category: 'Belajar',
+                priority: 'Sedang',
+                description: '',
+                deadline: new Date(),
+                createdAt: new Date(),
+                updatedAt: null,
+            } as Item)
+
+            // Reset state errors form
+            Object.assign(errors, {
+                title: undefined,
+                category: undefined,
+                priority: undefined,
+                deadline: undefined,
+                description:undefined,
+                createdAt: undefined,
+                updatedAt: undefined
+            } as ZodError)
+            
+        } catch (error :unknown) {
+            if (error instanceof z.ZodError ) {
+                handleZodError(error, errors );
+            }
+        } finally {
+            isSubmit.value = false;
+        }
+    }
+ 
+    return {
+        form,
+        isSubmit,
+        onSubmit,
+        errors
+    }
+}
