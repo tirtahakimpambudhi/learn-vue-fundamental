70656a4e515a306542ff1aa7f903493ca827363a
tirtahakimpambudhi <awebthp@gmail.com>
2025-07-19 16:03:17 +0700

    feat(src): :sparkles: Update src in the file main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,36 @@
 import { createApp } from 'vue'
 import './style.css'
+import { createPinia } from 'pinia';
+import App from '@pages/App.vue';
+import todolistRoute from '@todolist/route/index';
+import learnRoute from '@learn/route/index';
+import { createWebHistory, createRouter } from 'vue-router';
+import NotFound from '@/pages/NotFound.vue';
+import Home from '@pages/Home.vue';
+
+const pinia = createPinia();
+const app = createApp(App);
+
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: Home ,
+            name: 'Home'
+        },
+        ...todolistRoute,
+        ...learnRoute,
+        {
+            path: '/:pathMatch(.*)*',
+            component: NotFound,
+            name: 'NotFound'
+        }
+    ]
+})
+
 // import App from '@pages/App.vue'
 //
 // createApp(App).mount('#app')
@@ -17,5 +48,7 @@ import './style.css'
 
 // import Form from "@pages/Form.vue";
 // import Computed from "@pages/Computed.vue";
-import Todolist from "@pages/Todolist.vue";
-createApp(Todolist).mount('#app');
+
+app.use(pinia)
+app.use(router)
+app.mount('#app');
